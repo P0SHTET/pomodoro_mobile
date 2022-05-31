080708ba27d45051c71ef7d2bc7a866aa75c8816
galoperidol08@gmail.com <galoperidol08@gmail.com>
2022-05-31 15:19:24 +1000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:pomodoro_mobile/AccountPage.dart';
+import 'package:pomodoro_mobile/HomePage.dart';
+
+import 'InfoPage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        brightness: Brightness.dark,
+      ),
+      home: const MyHomePage(title: 'Pomodoro'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 1;
+  final List<Widget> _widgetOptions = [
+    const AccountPage(),
+    const HomePage(),
+    const InfoPage(),
+  ];
+
+  void _onNavigationItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.shifting,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle_rounded),
+            label: 'Account',
+            backgroundColor: Colors.white12,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+            backgroundColor: Colors.white10,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.info),
+            label: 'Info',
+            backgroundColor: Colors.white10,
+          )
+        ],
+        selectedItemColor: Colors.deepPurpleAccent,
+        unselectedItemColor: Colors.white70,
+        selectedLabelStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+        currentIndex: _selectedIndex,
+        onTap: _onNavigationItemTapped,
+      ),
+    );
+  }
+}

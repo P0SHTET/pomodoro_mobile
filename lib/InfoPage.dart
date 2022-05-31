2a12a8f4a4b9ce119abefacf31e65a12addbd5a2
galoperidol08@gmail.com <galoperidol08@gmail.com>
2022-05-31 16:28:17 +1000

    add: InfoPage

--- lib/InfoPage.dart
+++ lib/InfoPage.dart
@@ -1,15 +1,49 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
-class InfoPage extends StatefulWidget {
-  const InfoPage({Key? key}) : super(key: key);
-
-  @override
-  State<InfoPage> createState() => _InfoPageState();
-}
-
-class _InfoPageState extends State<InfoPage> {
+class InfoPage extends StatelessWidget {
+  static const description =
+      '«Метод помидора» — техника управления временем. Методика предполагает увеличение эффективности работы при меньших временных затратах за счёт глубокой концентрации и коротких перерывов. \n';
   @override
   Widget build(BuildContext context) {
-    return Text('Info');
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            description,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20,
+            ),
+          ),
+          RichText(
+            textAlign: TextAlign.center,
+            text: TextSpan(
+              children: [
+                TextSpan(
+                    text:
+                        'Данное приложение созданно с помощью Flutter\. Ознакомиться с исходным кодом можно в '),
+                TextSpan(
+                  text: 'репозитории на GitHub',
+                  style: TextStyle(
+                    color: Colors.deepPurpleAccent,
+                    decoration: TextDecoration.underline,
+                  ),
+                  recognizer: TapGestureRecognizer()
+                    ..onTap = () => launchUrlString('https://github.com/P0SHTET/pomodoro_mobile'),
+                )
+              ],
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -36,9 +36,9 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _selectedIndex = 1;
   final List<Widget> _widgetOptions = [
-    const AccountPage(),
-    const HomePage(),
-    const InfoPage(),
+    AccountPage(),
+    HomePage(),
+    InfoPage(),
   ];
 
   void _onNavigationItemTapped(int index) {
